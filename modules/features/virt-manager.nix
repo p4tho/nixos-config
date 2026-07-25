f4e73bb84ee2bacf782a68d35115720fa77bf3c7
p4tho <p4tho04@gmail.com>
2026-07-25 16:53:48 -0700

    fixed laggy screen for vms

--- modules/features/virt-manager.nix
+++ modules/features/virt-manager.nix
@@ -1,6 +1,15 @@
 { ... }: {
   flake.nixosModules.virt-manager = { pkgs, lib, ... }: {
     programs.virt-manager.enable = true;
-    virtualisation.libvirtd.enable = true;
+
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
+    };
+
+    # Override qemu_full to disable the broken ceph dependency
+    environment.systemPackages = with pkgs; [
+      (qemu_full.override { cephSupport = false; })
+    ];
   };
 }
